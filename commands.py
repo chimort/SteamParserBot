5e222e44620cb54d1173923f4308384c461ba5a2
Chimort <chimort@mail.ru>
2022-04-20 22:04:23 +0600

    .

--- commands.py
+++ commands.py
@@ -29,6 +29,7 @@ def parse(data):
     for game in games:
         title = game.find('span', {'class': 'title'}).text
         start_price = game.find('div', {'class': 'search_price'}).text.strip().split('$')[0]
+        print(game.find('div', {'class': 'search_price'}).text.strip().split('$'))
         discount = game.find('div', {'class': 'search_discount'}).text
         link = game.get('href')
         try:
